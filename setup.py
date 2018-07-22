6c3aab8686982b47fc8a0c10f84ac10de54a9031
Matija Kolarić <matija@jako.sexy>
2018-07-22 20:52:11 +0200

    setup changes

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 import setuptools
 
-with open("README.rst", "r") as fh:
+with open('README.rst', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="django-music-publisher",
-    version="0.0.2",
-    author="Matija Kolarić",
-    description="Django App for Music Publishers",
+    name='django-music-publisher',
+    version='0.0.4',
+    author='Matija Kolarić',
+    license='MIT License',
+    description='Django App for Music Publishers',
     long_description=long_description,
-    long_description_content_type="text/x-rst",
-    url="https://github.com/matijakolaric-com/django-music-publisher/",
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/matijakolaric-com/django-music-publisher/',
+    packages=setuptools.find_packages(exclude=['dmp_project']),
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Environment :: Web Environment',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
     ),
 )
