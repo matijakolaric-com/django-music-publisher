bcf1ad6c9813e2f5ee5ca935239c8facc86825a6
Matija Kolarić <matija@jako.sexy>
2018-07-24 15:48:04 +0200

    model tests finished

--- music_publisher/tests.py
+++ music_publisher/tests.py
@@ -2,12 +2,21 @@ from django.test import TestCase
 from datetime import date
 from music_publisher.models import *
 from django.core.exceptions import ValidationError
+import json
 
 
 class ModelsTest(TestCase):
     def setUp(self):
         self.artist = Artist(last_name="FOO", first_name="")
         self.artist.save()
+        self.thiswriter = Writer(
+            last_name='FOOBAR', first_name='JACK', ipi_name='199',
+            ipi_base='I1234567893', pr_society='010')
+        self.thiswriter.clean()
+        self.thiswriter.save()
+        self.otherwriter = Writer(last_name='DOE', first_name='JANE')
+        self.otherwriter.clean()
+        self.otherwriter.save()
         self.album_cd = AlbumCD(
             album_title='ALBUM', cd_identifier='XXX 123',
             release_date=date.today())
@@ -15,12 +24,20 @@ class ModelsTest(TestCase):
 
         self.work = Work(title='THE TITLE', iswc='T-123.456.789-4')
         self.work.save()
+        ArtistInWork(artist=self.artist, work=self.work).save()
+        WriterInWork(
+            writer=self.thiswriter, work=self.work, saan='SAMOSAN',
+            controlled=True, relative_share=50, capacity='CA').save()
+        WriterInWork(
+            writer=self.otherwriter, work=self.work, relative_share=50,
+            controlled=False, capacity='CA').save()
         self.alternate_title = AlternateTitle(
             work=self.work, title='The Alternate Title')
         self.alternate_title.save()
         self.work.firstrecording = FirstRecording(
             work=self.work, isrc='US-123-18-10000')
         self.work.firstrecording.save()
+        self.work.clean()
 
     def test_validation(self):
         self.assertTrue(VALIDATE)
@@ -35,8 +52,53 @@ class ModelsTest(TestCase):
             self.artist.validate_fields({'bad': 'dict'})
         self.work.firstrecording.clean_fields()
         self.assertEqual(self.work.firstrecording.isrc, 'US1231810000')
+        self.thiswriter.clean_fields()
+        self.assertTrue(self.thiswriter._can_be_controlled)
+        self.assertFalse(self.otherwriter._can_be_controlled)
+        with self.assertRaises(ValidationError) as ve:
+            self.otherwriter.saan = 'S4N1LJ4V4'
+            self.otherwriter.clean()
+        self.assertIn('saan', ve.exception.message_dict)
+        with self.assertRaises(ValidationError) as ve:
+            self.otherwriter.saan = None
+            self.otherwriter.generally_controlled = True
+            self.otherwriter.clean()
+        self.assertIn('generally_controlled', ve.exception.message_dict)
+        with self.assertRaises(ValidationError) as ve:
+            self.thiswriter.saan = 'S4N1LJ4V4'
+            self.thiswriter.clean()
+        self.assertIn('saan', ve.exception.message_dict)
+        with self.assertRaises(ValidationError) as ve:
+            self.thiswriter.ipi_name = None
+            self.thiswriter.saan = None
+            self.thiswriter.clean()
+        self.assertFalse(hasattr(ve.exception, 'message_dict'))
+        wiws = self.work.writerinwork_set.all()
+        for wiw in wiws:
+            if not wiw.controlled:
+                with self.assertRaises(ValidationError) as ve:
+                    wiw.writer.generally_controlled = True
+                    wiw.clean()
+                self.assertIn('controlled', ve.exception.message_dict)
+            else:
+                with self.assertRaises(ValidationError) as ve:
+                    wiw.capacity = None
+                    wiw.clean()
+                self.assertIn('capacity', ve.exception.message_dict)
+                wiw.capacity = 'CA'
+                with self.assertRaises(ValidationError) as ve:
+                    wiw.writer._can_be_controlled = False
+                    wiw.clean()
+                self.assertFalse(hasattr(ve.exception, 'message_dict'))
+                with self.assertRaises(ValidationError) as ve:
+                    wiw.writer = None
+                    wiw.clean()
+                self.assertIn('writer', ve.exception.message_dict)
 
     def test_str(self):
+        self.assertEqual(str(self.artist), 'FOO')
+        self.artist.first_name = "BAR JR"
+        self.assertEqual(str(self.artist), 'BAR JR FOO')
         self.assertEqual(str(self.alternate_title), 'THE ALTERNATE TITLE')
         self.assertEqual(str(self.album_cd), 'ALBUM (XXX 123)')
         self.assertEqual(self.album_cd.album_label, SETTINGS.get('label'))
@@ -52,6 +114,18 @@ class ModelsTest(TestCase):
             self.album_cd.clean()
         self.assertIn('cd_identifier', ve.exception.message_dict)
         self.assertIn('album_title', ve.exception.message_dict)
+        self.assertEqual(str(self.thiswriter), 'JACK FOOBAR')
+        self.thiswriter.generally_controlled = True
+        self.assertEqual(str(self.thiswriter), 'JACK FOOBAR (*)')
+        self.assertEqual(str(self.work.firstrecording), str(self.work))
+        aiw = self.work.artistinwork_set.first()
+        self.assertEqual(str(aiw), 'FOO')
+        wiw = self.work.writerinwork_set.first()
+        self.assertEqual(str(wiw), str(wiw.writer))
+
+    def test_json(self):
+        dump = json.dumps(self.work.json, indent=4)
+        self.assertIn('"SAMOSAN"', dump)
 
 
 FIELDS = {
