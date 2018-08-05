24e61a1718210b5e1671596468fd9de6a11bc4d3
Matija Kolarić <matija@jako.sexy>
2018-08-05 23:57:58 +0200

    setup.py version changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools.setup(
     name='django-music-publisher',
-    version='18.7',
+    version='18.7.1',
     author='Matija Kolarić',
     author_email='i@matijakolaric.com',
     license='MIT License',
