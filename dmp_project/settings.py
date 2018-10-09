9fbf3f465149d560d0d08f32d256aaec7a1a1f22
matijakolaric <matijakolaric@users.noreply.github.com>
2018-10-09 14:23:26 +0200

    Update settings.py

--- dmp_project/settings.py
+++ dmp_project/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.getenv('STATIC_ROOT', None),
+STATIC_ROOT = os.getenv('STATIC_ROOT', os.path.join(BASE_DIR, "static"))
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "dmp_project", "static"),
