ebc31b60a27f9be7650e0960caf32e846f971fc8
matijakolaric <matijakolaric@users.noreply.github.com>
2018-10-09 18:37:03 +0200

    Update settings.py

--- dmp_project/settings.py
+++ dmp_project/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'dmp_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(default='sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+DATABASES = {'default': dj_database_url.config(default='sqlite:///{}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))}
 
 
 # Password validation
