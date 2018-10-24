4db3e6ee51b11e548d5e6a2632add64d63909fb9
matijakolaric <matijakolaric@users.noreply.github.com>
2018-10-24 19:20:00 +0200

    Updated version to 18.9.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools.setup(
     name='django-music-publisher',
-    version='18.9.1',
+    version='18.9.2',
     author='Matija Kolarić',
     author_email='i@matijakolaric.com',
     license='MIT License',
