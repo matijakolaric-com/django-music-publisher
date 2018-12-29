1008827d9102eca09908c9e896f4c4693e89c204
matijakolaric <matijakolaric@users.noreply.github.com>
2018-12-29 19:33:28 +0100

    Update cwr_filters.py

--- music_publisher/templatetags/cwr_filters.py
+++ music_publisher/templatetags/cwr_filters.py
@@ -8,42 +8,9 @@ from django import template
 from datetime import date, time
 from decimal import Decimal
 from django.utils.html import mark_safe
-# from unidecode import unidecode
 
 
 register = template.Library()
-
-
-# @register.filter(name='booleanfield')
-# def booleanfield(value, length):
-#     """Format and validate boolean fields.
-
-#     Note that non-required flag fields are treated as boolean."""
-
-#     if value is None or value == '':
-#         value = ' '
-#     elif value is False:
-#         value = 'N'
-#     elif value is True:
-#         value = 'Y'
-#     if len(value) > length:
-#         raise AttributeError('Field value too long: {}'.format(value))
-#     return value.ljust(length).upper()
-
-
-# @register.filter(name='flagfield')
-# def flagfield(value, length):
-#     """Format and validate required flag fields."""
-
-#     if value is None or value == '':
-#         value = 'U'
-#     elif value is False:
-#         value = 'N'
-#     elif value is True:
-#         value = 'Y'
-#     if len(value) > length:
-#         raise AttributeError('Field value too long: {}'.format(value))
-#     return value.ljust(length).upper()
 
 
 @register.filter(name='rjust')
