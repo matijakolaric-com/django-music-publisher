bb417b52ce207a28ceaf0bc9a21e7041078b11d3
matijakolaric <matija@jako.sexy>
2019-01-22 16:11:45 +0100

    19.1.1b2

--- music_publisher/cwr_templates.py
+++ music_publisher/cwr_templates.py
@@ -9,17 +9,17 @@ from django.template import Template
 
 TEMPLATES_21 = {
     'GRH': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'GRH{{ transaction_type|ljust:3 }}0000102.10'
         '0000000000  \r\n{% endautoescape %}'),
     'HDR': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'HDRPB{{ publisher_ipi_name|slice:"2:" }}'
         '{{ publisher_name|ljust:45 }}01.10{{ creation_date|date:"Ymd" }}'
         '{{ creation_date|date:"His" }}{{ creation_date|date:"Ymd" }}'
         '               \r\n{% endautoescape %}'),
     'NWR': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         '{{ record_type }}'
         '{{ transaction_sequence|rjust:8 }}00000000'
         '{{ work_title|ljust:60 }}  {{ work_id|ljust:14 }}'
@@ -29,7 +29,7 @@ TEMPLATES_21 = {
         ' ' * 51 + 'N'
         '\r\n{% endautoescape %}'),
     'SPU': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'SPU{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ sequence|rjust:2 }}'
         '{{ publisher_id|ljust:9 }}{{ publisher_name|ljust:45 }}'
@@ -40,13 +40,13 @@ TEMPLATES_21 = {
         ' N {{ publisher_ipi_base|ljust:13 }}                               '
         '\r\n{% endautoescape %}'),
     'SPT': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'SPT{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ publisher_id|ljust:9 }}'
         '      {{ share|prshare }}{{ share|mrshare }}{{ share|mrshare }}'
         'I2136N001\r\n{% endautoescape %}'),
     'SWR': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'SWR{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ interested_party_number|ljust:9 }}'
         '{{ last_name|ljust:45 }}{{ first_name|ljust:30 }} '
@@ -55,18 +55,18 @@ TEMPLATES_21 = {
         '   00000   00000 N  {{ ipi_base|ljust:13 }}             \r\n'
         '{% endautoescape %}'),
     'SWT': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'SWT{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ interested_party_number|ljust:9 }}'
         '{{ share|prshare }}0000000000I2136N001\r\n{% endautoescape %}'),
     'PWR': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'PWR{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ publisher_id|ljust:9 }}'
         '{{ publisher_name|ljust:45 }}              {{ saan|ljust:14 }}'
         '{{ interested_party_number|ljust:9 }}\r\n{% endautoescape %}'),
     'OPU': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'OPU{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ sequence|rjust:2 }}' +
         ' ' * 54 +
@@ -77,7 +77,7 @@ TEMPLATES_21 = {
         ' N                                             '
         '\r\n{% endautoescape %}'),
     'OWR': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'OWR{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ interested_party_number|ljust:9 }}'
         '{{ last_name|ljust:45 }}{{ first_name|ljust:30 }}'
@@ -88,24 +88,24 @@ TEMPLATES_21 = {
         '{{ sr_society|ljust:3 }}{{ share|mrshare }}    '
         '{{ ipi_base|ljust:13 }}             \r\n{% endautoescape %}'),
     'ALT': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'ALT{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ alternate_title|ljust:60 }}AT  '
         '\r\n{% endautoescape %}'),
     'VER': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'VER{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ original_title|ljust:60 }}' +
         ' ' * (11 + 2 + 45 + 30 + 60 + 11 + 13 + 45 + 30 + 11 + 13 + 14) +
         '\r\n{% endautoescape %}'),
     'PER': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'PER{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}{{ last_name|ljust:45 }}'
         '{{ first_name|ljust:30 }}                        \r\n'
         '{% endautoescape %}'),
     'REC': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'REC{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}'
         '{{ release_date|date:"Ymd"|default:"00000000" }}' +
@@ -114,18 +114,18 @@ TEMPLATES_21 = {
         '{{ album_label|ljust:60 }}                  {{ ean|ljust:13 }}'
         '{{ isrc|ljust:12 }}     \r\n{% endautoescape %}'),
     'ORN': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'ORN{{ transaction_sequence|rjust:8 }}'
         '{{ record_sequence|rjust:8 }}LIB' + ' ' * 60 +
         '{{ cd_identifier|ljust:15 }}0000{{ library|ljust:60 }}' +
         ' ' * (26 + 12 + 60 + 20) + '0000                  \r\n'
         '{% endautoescape %}'),
     'GRT': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'GRT00001{{ transaction_count|rjust:8 }}'
         '{{ record_count|rjust:8 }}   0000000000\r\n{% endautoescape %}'),
     'TRL': Template(
-        '{% load cwr_filters %}{% autoescape on %}'
+        '{% load cwr_filters %}{% autoescape off %}'
         'TRL00001{{ transaction_count|rjust:8 }}'
         '{{ record_count|rjust:8 }}{% endautoescape %}'),
 }
