11c2b7b8aeab036fc4635f1a421626d7e333c333
matijakolaric <matija@jako.sexy>
2019-01-23 17:25:08 +0100

    acknowledgement-related filters

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools.setup(
     name='django-music-publisher',
-    version='19.1.1r1',
+    version='19.1.1rc1',
     author='Matija Kolarić',
     author_email='matijakolaric@users.noreply.github.com',
     license='MIT License',
