ab21aa7187480910fc3f22bb754f219a08a002ee
matijakolaric <matijakolaric@users.noreply.github.com>
2019-02-13 14:58:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools.setup(
     name='django-music-publisher',
-    version='19.1.2',
+    version='19.1.3',
     author='Matija Kolarić',
     author_email='matijakolaric@users.noreply.github.com',
     license='MIT License',
