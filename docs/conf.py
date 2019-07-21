14c3ed0507e37684d9e788be26dfa74f1eca2b2c
matijakolaric <matijakolaric@users.noreply.github.com>
2019-07-21 12:18:08 +0200

    19.1.5

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = 'Matija Kolarić'
 # The short X.Y version
 version = '19.1 Epiphany'
 # The full version, including alpha/beta/rc tags
-release = '19.1.4.post1'
+release = '19.1.5'
 
 rst_epilog = '''
 .. |version| replace:: {}
