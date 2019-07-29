c9421ea76aee2570beddfbd0f46062303721696c
matijakolaric <matijakolaric@users.noreply.github.com>
2019-07-29 11:22:20 +0200

    latex changes

--- docs/conf.py
+++ docs/conf.py
@@ -136,10 +136,9 @@ latex_elements = {
 
     # Latex figure (float) alignment
     #
-    'figure_align': 'htbp',
+    'figure_align': 'H',
 }
 
-latex_toplevel_sectioning = 'part'
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
