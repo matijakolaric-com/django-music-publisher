f891a2287d2176807b1277b36a1138b3474ae74a
matijakolaric <matijakolaric@users.noreply.github.com>
2019-12-02 18:11:03 +0100

    a single migration

--- dmp_project/settings.py
+++ dmp_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 
