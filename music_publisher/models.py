1cdd0fbed738e168e2c7d0f0c8cd510ce3085103
matijakolaric <matijakolaric@users.noreply.github.com>
2020-05-09 13:14:02 +0200

    squashed and removed old migrations, for versions older than 20, uprate must be gradual.

--- music_publisher/models.py
+++ music_publisher/models.py
@@ -1501,10 +1501,10 @@ class WorkAcknowledgement(models.Model):
     status = models.CharField(max_length=2, choices=TRANSACTION_STATUS_CHOICES)
     remote_work_id = models.CharField(max_length=20, blank=True)
 
-    def __str__(self):
-        return '{}: {}'.format(
-            self.work_id,
-            self.get_status_display())
+    # def __str__(self):
+    #     return '{}: {}'.format(
+    #         self.work_id,
+    #         self.get_status_display())
 
     def get_dict(self):
         """
