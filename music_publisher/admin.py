53e3d56039db6e5e9a33b636cf2f913b80693041
matijakolaric <matijakolaric@users.noreply.github.com>
2020-06-05 07:06:04 +0200

    Update admin.py

--- music_publisher/admin.py
+++ music_publisher/admin.py
@@ -1510,7 +1510,7 @@ class ACKImportForm(ModelForm):
         fields = ('acknowledgement_file',)
 
     acknowledgement_file = FileField()
-    import_iswcs = BooleanField(label='Import ISWCs if present')
+    import_iswcs = BooleanField(label='Import ISWCs if present', required=False)
 
     RE_HDR_21 = re.compile(
         r'^HDR(?:SO|AA)([ \d]{9})(.{45})01\.10(\d{8})\d{6}(\d{8})')
