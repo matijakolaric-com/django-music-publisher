46b7d255f2d63e13f8ca786dfe84aa4088792769
matijakolaric <matijakolaric@users.noreply.github.com>
2021-03-18 09:44:43 +0100

    improved tests

--- music_publisher/tests/tests.py
+++ music_publisher/tests/tests.py
@@ -282,6 +282,43 @@ class AdminTestNoNotes(TestCase):
     PUBLISHER_SOCIETY_SR='44',
     REQUIRE_SAAN=True,
     REQUIRE_PUBLISHER_FEE=True,
+    ENABLE_NOTES=True)
+class AdminTestWithNotes(TestCase):
+
+    fixtures = ['publishing_staff.json']
+
+    @classmethod
+    def setUpClass(cls):
+        """Class setup.
+
+        Creating staff user.
+        """
+        super().setUpClass()
+        cls.superuser = User.objects.create_superuser(
+            'superuser', '', 'password')
+
+    def test_with_notes(self):
+        self.client.force_login(self.superuser)
+        url = reverse(
+            'admin:music_publisher_writer_add')
+        response = self.client.get(url, follow=False)
+        self.assertEqual(response.status_code, 200)
+        url = reverse(
+            'admin:music_publisher_artist_add')
+        response = self.client.get(url, follow=False)
+        self.assertEqual(response.status_code, 200)
+
+
+@override_settings(
+    SECURE_SSL_REDIRECT=False,
+    PUBLISHER_NAME='TEST PUBLISHER',
+    PUBLISHER_CODE='MK',
+    PUBLISHER_IPI_NAME='0000000199',
+    PUBLISHER_SOCIETY_PR='52',
+    PUBLISHER_SOCIETY_MR='44',
+    PUBLISHER_SOCIETY_SR='44',
+    REQUIRE_SAAN=True,
+    REQUIRE_PUBLISHER_FEE=True,
     PUBLISHING_AGREEMENT_PUBLISHER_PR=Decimal('0.333333'),
     PUBLISHING_AGREEMENT_PUBLISHER_MR=Decimal('0.5'),
     PUBLISHING_AGREEMENT_PUBLISHER_SR=Decimal('0.75'),
