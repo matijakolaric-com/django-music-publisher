c0f8a96a4e70dc087479719c9b12c0fed02d47a2
matijakolaric <matijakolaric@users.noreply.github.com>
2022-06-02 11:28:24 +0200

    bumped version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst', 'r') as fh:
 
 setuptools.setup(
     name='django-music-publisher',
-    version='22.1',
+    version='22.1.6a1',
     author='Matija Kolarić',
     author_email='matijakolaric@users.noreply.github.com',
     license='MIT License',
