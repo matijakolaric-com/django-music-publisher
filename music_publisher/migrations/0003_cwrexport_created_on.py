90e3c2ecfe9d2b64d3cdc1bda308cf623f2678e6
matijakolaric <matijakolaric@users.noreply.github.com>
2023-03-30 15:19:41 +0200

    old migration removed

--- music_publisher/migrations/0003_cwrexport_created_on.py
+++ music_publisher/migrations/0003_cwrexport_created_on.py
@@ -4,20 +4,6 @@ from django.db import migrations, models
 from django.utils.timezone import datetime, make_aware, get_fixed_timezone
 
 
-def set_created_on(apps, schema_editor):
-    cls = apps.get_model("music_publisher", "CWRExport")
-    qs = cls.objects.filter(created_on__isnull=True)
-    for obj in qs:
-        if obj.nwr_rev in ["NWR", "NW2", "REV", "RE2"]:
-            s = obj.cwr[64:78]
-        else:
-            s = obj.cwr[65:79]
-        obj.created_on = make_aware(
-            datetime.strptime(s, "%Y%m%d%H%M%S"), get_fixed_timezone(0)
-        )
-        obj.save()
-
-
 class Migration(migrations.Migration):
     dependencies = [
         ("music_publisher", "0002_mayday"),
@@ -28,9 +14,5 @@ class Migration(migrations.Migration):
             model_name="cwrexport",
             name="created_on",
             field=models.DateTimeField(editable=False, null=True),
-        ),
-        migrations.RunPython(
-            code=set_created_on,
-            reverse_code=migrations.operations.special.RunPython.noop,
         ),
     ]
