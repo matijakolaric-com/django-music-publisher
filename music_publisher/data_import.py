ec19e86ac0b07e2d296162a567ba69d4f5e003f1
matijakolaric <matijakolaric@users.noreply.github.com>
2024-12-09 16:44:38 +0100

    Bugfix - not importing recording title, version title, etc. Blacked.

--- music_publisher/data_import.py
+++ music_publisher/data_import.py
@@ -495,9 +495,15 @@ class DataImporter(object):
                 duration=recording.get("duration"),
                 release_date=recording.get("release_date"),
                 recording_title=recording.get("recording_title", ""),
-                recording_title_suffix=str(recording.get("recording_title_suffix"))[0] in [True, 'Y'],
+                recording_title_suffix=str(
+                    recording.get("recording_title_suffix")
+                )[0]
+                in [True, "Y"],
                 version_title=recording.get("version_title", ""),
-                version_title_suffix=str(recording.get("version_title_suffix"))[0] in [True, 'Y'],
+                version_title_suffix=str(
+                    recording.get("version_title_suffix")
+                )[0]
+                in [True, "Y"],
             )
             recording.clean_fields()
             recording.clean()
