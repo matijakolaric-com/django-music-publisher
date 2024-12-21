dd7ce7b3be37fbe2838a580b0bfbab2405d5e22f
matijakolaric <matijakolaric@users.noreply.github.com>
2024-12-21 13:01:12 +0100

    24.12.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="django-music-publisher",
-    version="24.12.1a1",
+    version="24.12.1",
     author="Matija Kolarić",
     author_email="matijakolaric@users.noreply.github.com",
     license="MIT License",
