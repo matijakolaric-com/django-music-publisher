16320d4648b6fc9bce4b5bb0e6e252369f57eba0
matijakolaric <matijakolaric@users.noreply.github.com>
2026-04-11 19:31:25 +0200

    moved all custom templates to old_templates, crating new ones, only those that are necessary

--- music_publisher/cwr_templates.py
+++ music_publisher/cwr_templates.py
@@ -1,10 +1,10 @@
-"""Django templates for CWR generation.
+"""Django old_templates for CWR generation.
 
 Attributes:
-    TEMPLATES_21 (dict): Record templates for CWR 2.1
-    TEMPLATES_22 (dict): Record templates for CWR 2.2, based on 2.1
-    TEMPLATES_30 (dict): Record templates for CWR 3.0
-    TEMPLATES_31 (dict): Record templates for CWR 3.1, based on 3.0
+    TEMPLATES_21 (dict): Record old_templates for CWR 2.1
+    TEMPLATES_22 (dict): Record old_templates for CWR 2.2, based on 2.1
+    TEMPLATES_30 (dict): Record old_templates for CWR 3.0
+    TEMPLATES_31 (dict): Record old_templates for CWR 3.1, based on 3.0
 """
 
 from django.template import Template
