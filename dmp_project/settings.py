1884e8380da159ec99bf1d7f735fea3d78940db2
matijakolaric <matijakolaric@users.noreply.github.com>
2026-05-09 19:09:42 +0200

    splitting the large CWR export into 10k work chunks, manual generation.

--- dmp_project/settings.py
+++ dmp_project/settings.py
@@ -160,10 +160,14 @@ OPTION_FORCE_CASE = os.getenv("OPTION_FORCE_CASE")
 
 # CWR generation options
 # Exports below this number of works are generated immediately on save.
-OPTION_CWR_SYNC_WORK_LIMIT = int(os.getenv("OPTION_CWR_SYNC_WORK_LIMIT", "5000"))
+OPTION_CWR_SYNC_WORK_LIMIT = int(
+    os.getenv("OPTION_CWR_SYNC_WORK_LIMIT", "5000")
+)
 
 # Larger CWR requests are split into files with at most this many works.
-OPTION_CWR_WORKS_PER_FILE = int(os.getenv("OPTION_CWR_WORKS_PER_FILE", "10000"))
+OPTION_CWR_WORKS_PER_FILE = int(
+    os.getenv("OPTION_CWR_WORKS_PER_FILE", "10000")
+)
 
 # REMOTE FILES
 # The default is Digital Ocean Spaces, but any S3 should work with AWS
